7b86b4ac65476d5e20cc86ba470cd6c1148306a1
Mostafa Farghaly <mostafa@kotal.co>
2021-03-09 15:21:02 +0200

    created api v1 ethereum nodes basic handler

---
+++ handlers/ethereum.go
@@ -0,0 +1,40 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+// EthereumHandler is Ethereum handler
+type EthereumHandler struct{}
+
+// Get gets a single node
+func (e *EthereumHandler) Get(c *fiber.Ctx) error {
+	return c.SendString("Get a node")
+}
+
+// List lists all Ethereum nodes
+func (e *EthereumHandler) List(c *fiber.Ctx) error {
+	return c.SendString("List all nodes")
+}
+
+// Create creates a single Ethereum node from spec
+func (e *EthereumHandler) Create(c *fiber.Ctx) error {
+	return c.SendString("Create a node")
+}
+
+// Delete deletes a single Ethereum node by name
+func (e *EthereumHandler) Delete(c *fiber.Ctx) error {
+	return c.SendString("Delete a node")
+}
+
+// Update updates a single node by name from new spec delta
+func (e *EthereumHandler) Update(c *fiber.Ctx) error {
+	return c.SendString("Update a node")
+}
+
+// Register registers all routes on the given router
+func (e *EthereumHandler) Register(router fiber.Router) {
+	router.Post("/", e.Create)
+	router.Get("/", e.List)
+	router.Get("/:name", e.Get)
+	router.Put("/:name", e.Update)
+	router.Delete("/:name", e.Delete)
+}
--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/kotalco/api/handlers"
 )
 
 func main() {
 	app := fiber.New()
 
+	api := app.Group("api")
+	v1 := api.Group("v1")
+	ethereum := v1.Group("ethereum")
+	nodes := ethereum.Group("nodes")
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Kotal API")
 	})
+
+	ethereumHandler := handlers.EthereumHandler{}
+	ethereumHandler.Register(nodes)
 
 	app.Listen(":3000")
 }
