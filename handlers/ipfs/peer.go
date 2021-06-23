4877b553903507d9dc714741727668e05ff66fcd
Mostafa Farghaly <mostafa@kotal.co>
2021-06-24 00:25:16 +0200

    create k8s ipfs peer

--- handlers/ipfs/peer.go
+++ handlers/ipfs/peer.go
@@ -1,8 +1,17 @@
 package handlers
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/kotalco/api/handlers"
+	"github.com/kotalco/api/k8s"
+	models "github.com/kotalco/api/models/ipfs"
+	ipfsv1alpha1 "github.com/kotalco/kotal/apis/ipfs/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // PeerHandler is IPFS peer handler
@@ -25,7 +34,47 @@ func (p *PeerHandler) List(c *fiber.Ctx) error {
 
 // Create creates IPFS peer from spec
 func (p *PeerHandler) Create(c *fiber.Ctx) error {
-	return c.SendString("Create a peer")
+	model := new(models.Peer)
+
+	if err := c.BodyParser(model); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": "bad request",
+		})
+	}
+
+	initProfiles := []ipfsv1alpha1.Profile{}
+	for _, profile := range model.InitProfiles {
+		initProfiles = append(initProfiles, ipfsv1alpha1.Profile(profile))
+	}
+
+	peer := &ipfsv1alpha1.Peer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      model.Name,
+			Namespace: "default",
+		},
+		Spec: ipfsv1alpha1.PeerSpec{
+			InitProfiles: initProfiles,
+		},
+	}
+
+	if err := k8s.Client().Create(c.Context(), peer); err != nil {
+		log.Println(err)
+
+		if errors.IsAlreadyExists(err) {
+			return c.Status(http.StatusUnprocessableEntity).JSON(fiber.Map{
+				"error": fmt.Sprintf("peer by name %s already exist", model.Name),
+			})
+		}
+
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to create peer",
+		})
+
+	}
+
+	return c.Status(http.StatusCreated).JSON(fiber.Map{
+		"peer": models.FromIPFSPeer(peer),
+	})
 }
 
 // Delete deletes IPFS peer by name
