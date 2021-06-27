413ae5b6810ba75041ac8c473b3b5445292eb043
Mostafa Farghaly <mostafa@kotal.co>
2021-06-27 20:12:00 +0200

    create k8s ethereum 2.0 validator

--- handlers/ethereum2/validator.go
+++ handlers/ethereum2/validator.go
@@ -1,8 +1,17 @@
 package handlers
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/kotalco/api/handlers"
+	"github.com/kotalco/api/k8s"
+	models "github.com/kotalco/api/models/ethereum2"
+	ethereum2v1alpha1 "github.com/kotalco/kotal/apis/ethereum2/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // ValidatorHandler is Ethereum 2.0 validator client handler
@@ -25,7 +34,59 @@ func (p *ValidatorHandler) List(c *fiber.Ctx) error {
 
 // Create creates Ethereum 2.0 validator client from spec
 func (p *ValidatorHandler) Create(c *fiber.Ctx) error {
-	return c.SendString("Create a validator client")
+	model := new(models.Validator)
+
+	if err := c.BodyParser(model); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"error": "bad request",
+		})
+	}
+
+	keystores := []ethereum2v1alpha1.Keystore{}
+	for _, keystore := range model.Keystores {
+		keystores = append(keystores, ethereum2v1alpha1.Keystore{
+			SecretName: keystore.SecretName,
+		})
+	}
+
+	validator := &ethereum2v1alpha1.Validator{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      model.Name,
+			Namespace: "default",
+		},
+		Spec: ethereum2v1alpha1.ValidatorSpec{
+			Network:   model.Network,
+			Client:    ethereum2v1alpha1.Ethereum2Client(model.Client),
+			Keystores: keystores,
+		},
+	}
+
+	if model.Client == string(ethereum2v1alpha1.PrysmClient) && model.WalletPasswordSecretName != "" {
+		validator.Spec.WalletPasswordSecret = model.WalletPasswordSecretName
+	}
+
+	if len(model.BeaconEndpoints) != 0 {
+		validator.Spec.BeaconEndpoints = model.BeaconEndpoints
+	} else {
+		validator.Spec.BeaconEndpoints = []string{}
+	}
+
+	if err := k8s.Client().Create(c.Context(), validator); err != nil {
+		log.Println(err)
+		if errors.IsAlreadyExists(err) {
+			return c.Status(http.StatusUnprocessableEntity).JSON(fiber.Map{
+				"error": fmt.Sprintf("validator by name %s already exist", model.Name),
+			})
+		}
+
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to create validator",
+		})
+	}
+
+	return c.Status(http.StatusOK).JSON(fiber.Map{
+		"validator": models.FromEthereum2Validator(validator),
+	})
 }
 
 // Delete deletes Ethereum 2.0 validator client by name
