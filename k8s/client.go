94c30d3989cf779f14995e9065348fbd2f7b6088
Mostafa Farghaly <mostafa@kotal.co>
2022-01-02 19:15:53 +0200

    update test environment crd directory path

--- k8s/client.go
+++ k8s/client.go
@@ -62,7 +62,7 @@ func Config() (*rest.Config, error) {
 		log.Println("creating k8s client using test environment ...")
 		testEnv := envtest.Environment{
 			CRDDirectoryPaths: []string{
-				filepath.Join(build.Default.GOPATH, "pkg", "mod", "github.com", "kotalco", "kotal@v0.0.0-20211225234345-b07aa1a91d79", "config", "crd", "bases"),
+				filepath.Join(build.Default.GOPATH, "pkg", "mod", "github.com", "kotalco", "kotal@v0.0.0-20211230162042-e7dc4d1b39fb", "config", "crd", "bases"),
 			},
 			ErrorIfCRDPathMissing: true,
 		}
