d49f3f9e23a75e92795f5ad94d5c0d6bddb01ba0
Mostafa Farghaly <mostafa@kotal.co>
2022-01-31 02:25:54 +0200

    stream near node stats over websocket

--- handlers/near/node.go
+++ handlers/near/node.go
@@ -1,12 +1,16 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
 	"os"
 	"sort"
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
@@ -17,6 +21,7 @@ import (
 	"github.com/kotalco/api/shared"
 	nearv1alpha1 "github.com/kotalco/kotal/apis/near/v1alpha1"
 	sharedAPIs "github.com/kotalco/kotal/apis/shared"
+	"github.com/ybbus/jsonrpc/v2"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -242,6 +247,126 @@ func (n *NodeHandler) Count(c *fiber.Ctx) error {
 	return c.SendStatus(http.StatusOK)
 }
 
+func (e *NodeHandler) Stats(c *websocket.Conn) {
+	defer c.Close()
+
+	type Result struct {
+		Error string `json:"error,omitempty"`
+		// network_info call
+		ActivePeersCount       uint `json:"activePeersCount,omitempty"`
+		MaxPeersCount          uint `json:"maxPeersCount,omitempty"`
+		SentBytesPerSecond     uint `json:"sentBytesPerSecond,omitempty"`
+		ReceivedBytesPerSecond uint `json:"receivedBytesPerSecond,omitempty"`
+		// status call
+		LatestBlockHeight   uint `json:"latestBlockHeight,omitempty"`
+		EarliestBlockHeight uint `json:"earliestBlockHeight,omitempty"`
+		Syncing             bool `json:"syncing,omitempty"`
+	}
+
+	// Mock serever
+	if os.Getenv("MOCK") == "true" {
+		var activePeersCount, sentBytesPerSecond, receivedBytesPerSecond, latestBlockHeight, earliestBlockHeight uint
+		for {
+			activePeersCount++
+			sentBytesPerSecond += 100
+			receivedBytesPerSecond += 100
+			latestBlockHeight += 36
+			earliestBlockHeight += 3
+
+			r := &Result{
+				ActivePeersCount:       activePeersCount,
+				MaxPeersCount:          40,
+				SentBytesPerSecond:     sentBytesPerSecond,
+				ReceivedBytesPerSecond: receivedBytesPerSecond,
+				LatestBlockHeight:      latestBlockHeight,
+				EarliestBlockHeight:    earliestBlockHeight,
+				Syncing:                true,
+			}
+
+			var msg []byte
+
+			if activePeersCount > 40 {
+				activePeersCount = 10
+				r = &Result{
+					Error: "rpc is not enabled",
+				}
+			}
+
+			msg, _ = json.Marshal(r)
+			c.WriteMessage(websocket.TextMessage, []byte(msg))
+			time.Sleep(time.Second)
+		}
+	}
+
+	name := c.Params("name")
+	node := &nearv1alpha1.Node{}
+	key := types.NamespacedName{
+		Namespace: "default",
+		Name:      name,
+	}
+
+	for {
+
+		err := k8s.Client().Get(context.Background(), key, node)
+		if errors.IsNotFound(err) {
+			c.WriteJSON(fiber.Map{
+				"error": fmt.Sprintf("node by name %s doesn't exist", name),
+			})
+			return
+		}
+
+		if !node.Spec.RPC {
+			c.WriteJSON(fiber.Map{
+				"error": "rpc is not enabled",
+			})
+			return
+		}
+
+		client := jsonrpc.NewClient(fmt.Sprintf("http://%s:%d", node.Name, node.Spec.RPCPort))
+
+		type NodeStatus struct {
+			SyncInfo struct {
+				LatestBlockHeight   uint `json:"latest_block_height"`
+				EarliestBlockHeight uint `json:"earliest_block_height"`
+				Syncing             bool `json:"syncing"`
+			} `json:"sync_info"`
+		}
+
+		// node status rpc call
+		nodeStatus := &NodeStatus{}
+		err = client.CallFor(nodeStatus, "status")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		type NetworkInfo struct {
+			ActivePeersCount       uint `json:"num_active_peers"`
+			MaxPeersCount          uint `json:"peer_max_count"`
+			SentBytesPerSecond     uint `json:"sent_bytes_per_sec"`
+			ReceivedBytesPerSecond uint `json:"received_bytes_per_sec"`
+		}
+
+		// network info rpc call
+		networkInfo := &NetworkInfo{}
+		err = client.CallFor(networkInfo, "network_info")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		c.WriteJSON(fiber.Map{
+			"activePeersCount":       networkInfo.ActivePeersCount,
+			"maxPeersCount":          networkInfo.MaxPeersCount,
+			"sentBytesPerSecond":     networkInfo.SentBytesPerSecond,
+			"receivedBytesPerSecond": networkInfo.ReceivedBytesPerSecond,
+			"latestBlockHeight":      nodeStatus.SyncInfo.LatestBlockHeight,
+			"earliestBlockHeight":    nodeStatus.SyncInfo.EarliestBlockHeight,
+			"syncing":                nodeStatus.SyncInfo.Syncing,
+		})
+
+		time.Sleep(time.Second)
+	}
+}
+
 // validateNodeExist validates NEAR node by name exist
 func validateNodeExist(c *fiber.Ctx) error {
 	name := c.Params("name")
@@ -277,6 +402,7 @@ func (n *NodeHandler) Register(router fiber.Router) {
 	router.Get("/:name", validateNodeExist, n.Get)
 	router.Get("/:name/logs", websocket.New(sharedHandlers.Logger))
 	router.Get("/:name/status", websocket.New(sharedHandlers.Status))
+	router.Get("/:name/stats", websocket.New(n.Stats))
 	router.Put("/:name", validateNodeExist, n.Update)
 	router.Delete("/:name", validateNodeExist, n.Delete)
 }
