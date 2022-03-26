8daad0032af4f9dd9ce08a8c9bd06502fa4c9b98
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-03-27 00:27:19 +0200

    ref:formatted rest_error file

--- pkg/errors/rest_errors.go
+++ pkg/errors/rest_errors.go
@@ -7,12 +7,12 @@ import (
 type RestErr struct {
 	Message     string            `json:"message"`
 	Status      int               `json:"status"`
-	Name      string            `json:"name"`
+	Name        string            `json:"name"`
 	Validations map[string]string `json:"validations,omitempty"`
 }
 
 //Error used to mimic build in error pkg so this can be replaceable  for go error pkg
-func (err RestErr)Error() string  {
+func (err RestErr) Error() string {
 	return err.Message
 }
 
@@ -20,37 +20,37 @@ func NewValidationError(validations map[string]string) *RestErr {
 	return &RestErr{
 		Message:     "Invalid Body Request",
 		Status:      http.StatusBadRequest,
-		Name:       "Bad Request",
+		Name:        "Bad Request",
 		Validations: validations,
 	}
 }
 func NewBadRequestError(message string) *RestErr {
 	return &RestErr{
-		Message:     message,
-		Status:      http.StatusBadRequest,
-		Name:       "Bad Request",
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Name:    "Bad Request",
 	}
 }
 func NewNotFoundError(message string) *RestErr {
 	return &RestErr{
-		Message:     message,
-		Status:      http.StatusNotFound,
-		Name:       "Not Found",
+		Message: message,
+		Status:  http.StatusNotFound,
+		Name:    "Not Found",
 	}
 }
 func NewInternalServerError(message string) *RestErr {
 	return &RestErr{
-		Message:     message,
-		Status:      http.StatusInternalServerError,
-		Name:       "Internal Server Error",
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Name:    "Internal Server Error",
 	}
 }
 
 func NewUnAuthorizedError(message string) *RestErr {
 	return &RestErr{
-		Message:     message,
-		Status:      http.StatusUnauthorized,
-		Name:       "UnAuthorized",
+		Message: message,
+		Status:  http.StatusUnauthorized,
+		Name:    "UnAuthorized",
 	}
 }
 
@@ -58,7 +58,7 @@ func NewForbiddenError(message string) *RestErr {
 	return &RestErr{
 		Message: message,
 		Status:  http.StatusForbidden,
-		Name:   "Forbidden",
+		Name:    "Forbidden",
 	}
 }
 
@@ -66,16 +66,14 @@ func NewTooManyRequestsError(message string) *RestErr {
 	return &RestErr{
 		Message: message,
 		Status:  http.StatusTooManyRequests,
-		Name:   "Too Many Requests",
+		Name:    "Too Many Requests",
 	}
 }
-
-
 
 func NewConflictError(message string) *RestErr {
 	return &RestErr{
 		Message: message,
 		Status:  http.StatusConflict,
-		Name:   "Conflict",
+		Name:    "Conflict",
 	}
 }
