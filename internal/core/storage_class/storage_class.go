edcf0b3f1edb5b04e832204ce78a47f1a0a179d2
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-05-08 01:56:59 +0200

    ref: remove ns from storage class since it's not namespaced object

--- internal/core/storage_class/storage_class.go
+++ internal/core/storage_class/storage_class.go
@@ -1,13 +1,11 @@
 package storage_class
 
 import (
-	"github.com/kotalco/api/pkg/k8s"
 	storagev1 "k8s.io/api/storage/v1"
 )
 
 // StorageClass is Kubernetes storage class
 type StorageClassDto struct {
-	k8s.MetaDataDto
 	Provisioner          string `json:"provisioner"`
 	ReclaimPolicy        string `json:"reclaimPolicy"`
 	AllowVolumeExpansion bool   `json:"allowVolumeExpansion"`
@@ -16,7 +14,6 @@ type StorageClassDto struct {
 type StorageClassListDto []StorageClassDto
 
 func (dto StorageClassDto) FromCoreStorageClass(sc *storagev1.StorageClass) *StorageClassDto {
-	dto.Name = sc.Name
 	dto.Provisioner = sc.Provisioner
 	dto.ReclaimPolicy = string(*sc.ReclaimPolicy)
 	dto.AllowVolumeExpansion = sc.AllowVolumeExpansion != nil && *sc.AllowVolumeExpansion
