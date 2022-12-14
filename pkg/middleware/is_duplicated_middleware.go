47f4773fcb816d6a23082fdc37c7a0f9f82e6f8c
mohamed-abdelrhman <mohamed95zoytak@gmail.com>
2022-12-14 17:10:00 +0200

    feat: remove isPost check from middleware since we use it for create methods only

--- pkg/middleware/is_duplicated_middleware.go
+++ pkg/middleware/is_duplicated_middleware.go
@@ -12,27 +12,25 @@ import (
 var statefulService = statefulset.NewService()
 
 func IsDuplicated(c *fiber.Ctx) error {
-	if c.Request().Header.IsPost() { // check on posts verbs only while trying to create new resource
-		var bodyFields map[string]interface{}
-		_ = c.BodyParser(&bodyFields)
-		if bodyFields["name"] != nil {
-			name := bodyFields["name"].(string)
-			record, err := statefulService.Get(types.NamespacedName{
-				Namespace: c.Locals("namespace", "default").(string),
-				Name:      name,
-			})
+	var bodyFields map[string]interface{}
+	_ = c.BodyParser(&bodyFields)
+	if bodyFields["name"] != nil {
+		name := bodyFields["name"].(string)
+		record, err := statefulService.Get(types.NamespacedName{
+			Namespace: c.Locals("namespace", "default").(string),
+			Name:      name,
+		})
 
-			if record != nil { //check if record already exist , return conflict if true
-				conflictErr := restError.NewConflictError(fmt.Sprintf("resource %s already exists!", name))
-				return c.Status(conflictErr.Status).JSON(conflictErr)
-			}
+		if record != nil { //check if record already exist , return conflict if true
+			conflictErr := restError.NewConflictError(fmt.Sprintf("resource %s already exists!", name))
+			return c.Status(conflictErr.Status).JSON(conflictErr)
+		}
 
-			if err != nil { //check if err code is notFound , pass if true, throw if any other erros
-				if err.Status == http.StatusNotFound {
-					return c.Next()
-				} else {
-					return c.Status(err.Status).JSON(err)
-				}
+		if err != nil { //check if err code is notFound , pass if true, throw if any other erros
+			if err.Status == http.StatusNotFound {
+				return c.Next()
+			} else {
+				return c.Status(err.Status).JSON(err)
 			}
 		}
 	}
