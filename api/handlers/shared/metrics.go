a3c1a87b896dc4923bcb6cdefe5549f7084db9c6
Mostafa Farghaly <keepondev@gmail.com>
2023-04-14 14:03:04 +0200

    Revert "feat: watch metrics"

--- api/handlers/shared/metrics.go
+++ api/handlers/shared/metrics.go
@@ -3,22 +3,28 @@ package shared
 import (
 	"context"
 	"fmt"
+	"time"
+
+	restError "github.com/kotalco/community-api/pkg/errors"
+	"github.com/kotalco/community-api/pkg/logger"
+	"github.com/kotalco/community-api/pkg/shared"
+	appsv1 "k8s.io/api/apps/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/gofiber/websocket/v2"
 	"github.com/kotalco/community-api/pkg/k8s"
-	"github.com/kotalco/community-api/pkg/logger"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/metrics/pkg/apis/metrics/v1beta1"
+	"k8s.io/apimachinery/pkg/types"
 )
+
+var metricsClientset = k8s.MetricsClientset()
 
 type metricsResponseDto struct {
 	Cpu    int64 `json:"cpu"`
 	Memory int64 `json:"memory"`
 }
-
-var metricsClientset = k8s.MetricsClientset()
 
 // Metrics returns a websocket that emits cpu and memory usage
 func Metrics(c *websocket.Conn) {
@@ -26,35 +32,53 @@ func Metrics(c *websocket.Conn) {
 
 	name := c.Params("name")
 	ns := c.Locals("namespace").(string)
+	pod := &corev1.Pod{}
 
-	// Create a new Metrics Query object to get the CPU and Memory usage of the pod
-	selector := fmt.Sprintf("app.kubernetes.io/managed-by=kotal-operator,app.kubernetes.io/instance=%s", name)
-
-	// Watch the Metrics of the pod using the Metrics API
-	watcher, err := metricsClientset.MetricsV1beta1().PodMetricses(ns).Watch(context.Background(), metav1.ListOptions{
-		LabelSelector: selector,
-	})
-	if err != nil {
-		go logger.Info("METRICS_STREAM", err.Error())
-		return
+	key := types.NamespacedName{
+		Namespace: ns,
+		Name:      fmt.Sprintf("%s-0", name),
 	}
 
-	for event := range watcher.ResultChan() {
-		if event.Type == corev1.StreamTypeError {
+	sts := &appsv1.StatefulSet{}
+	stsKey := types.NamespacedName{
+		Namespace: ns,
+		Name:      name,
+	}
+
+podCheck:
+	if err := k8sClient.Get(context.Background(), key, pod); err != nil {
+		go logger.Info("METRICS_POD_NOTFOUND", err.Error())
+		// is the pod error due to sts has been deleted ?
+		stsErr := k8sClient.Get(context.Background(), stsKey, sts)
+		if apierrors.IsNotFound(stsErr) {
+			go logger.Info("METRICS_STS_NOTFOUND", stsErr.Error())
+			c.WriteJSON(shared.NewResponse(restError.NewNotFoundError(stsErr.Error())))
 			return
 		}
+		time.Sleep(3 * time.Second)
+		goto podCheck
+	}
 
-		metrics, ok := event.Object.(*v1beta1.PodMetrics)
-		if !ok {
-			return
-		}
+	opts := metav1.GetOptions{}
+	podMetrics := metricsClientset.MetricsV1beta1().PodMetricses(key.Namespace)
 
+	for {
 		response := new(metricsResponseDto)
+
+		metrics, err := podMetrics.Get(context.Background(), key.Name, opts)
+		if err != nil {
+			go logger.Info("METRICS_API_ERR", err.Error())
+			time.Sleep(3 * time.Second)
+			goto podCheck
+		}
+
 		response.Cpu = metrics.Containers[0].Usage.Cpu().ScaledValue(resource.Milli)
 		response.Memory = metrics.Containers[0].Usage.Memory().ScaledValue(resource.Mega)
 
 		if err := c.WriteJSON(response); err != nil {
 			return
 		}
+
+		time.Sleep(time.Second)
 	}
 }
